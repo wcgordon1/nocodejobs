586276e0f9c20b5543b382158ba9df869eee4abb
wcgordon1 <wcgordon1@gmail.com>
2024-11-02 16:55:31 -0700

    this is the template, RSS, everything

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,26 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
 export async function GET(context) {
+  const jobs = await getCollection('jobs');
+  
   return rss({
-     title: 'Tustin Recruiting',
-    description: 'Tustin Recruiting is a boutique recruiting firm specializing in the placement of top talent in Orange County, California.',
+    title: 'Best Electrician Jobs',
+    description: 'Find the best electrician jobs - Commercial, Residential, and Industrial positions',
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./blog/*.{md,mdx}'),
-    ),
+    items: jobs.map((job) => ({
+      title: job.data.position,
+      link: `/jobs/${job.slug}`,
+      description: job.data.description,
+      pubDate: new Date(job.data.datePosted),
+      customData: `
+        <location>${job.data.location}</location>
+        <company>${job.data.hiringOrganization.name}</company>
+        <salary>$${job.data.baseSalary.minValue} - $${job.data.baseSalary.maxValue} ${job.data.baseSalary.unitText}</salary>
+        <employmentType>${job.data.employmentType}</employmentType>
+        <team>${job.data.team}</team>
+        <experienceRequirements>${job.data.experienceRequirements}</experienceRequirements>
+      `,
+    })),
   });
 }
